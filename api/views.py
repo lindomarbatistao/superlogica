754b20e7a219213dd9465aabd0ad93b54ce235d9
lindomar <lindomarbatistao@gmail.com>
2026-02-02 11:34:44 -0300

    URLs

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,3 +10,13 @@ from .serializers import (
     ContratoSerilizer, 
     PagamentoSerializer
 )
+
+# class UsuarioListCreateAPIView(APIView):
+#     def get(self, request):
+#         usuarios = Usuario.objects.all()
+#         serializer = UsuarioSerializer(usuarios, many=True)
+#         return Response(serializer.data)
+
+class UsuarioListCreateAPIView(ListCreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
