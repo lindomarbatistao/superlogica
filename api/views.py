40e17e76ba53cb9f62dceb091f8adcbd04964e2b
lindomar <lindomarbatistao@gmail.com>
2026-02-02 10:32:46 -0300

    Serializers+Views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Usuario, Imovel, Contrato, Pagamento
+from .serializers import (
+    UsuarioSerializer, 
+    ImovelSerializer, 
+    ContratoSerilizer, 
+    PagamentoSerializer
+)
